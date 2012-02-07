4ca24977c6ef285dbf781c851ef4aea9991db7e0
Cooper Maruyama <cooperm@susteen.com>
2012-02-07 15:20:35 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrea::Application.config.secret_token = '97e6a28012a240afe4cbd49addeebcb71edcc4061b38acaaf94de76d9c44e1513abda118d7c47aa7f589b2e7253955865743ca9f916124755cb33a1a206029cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mrea::Application.config.session_store :cookie_store, :key => '_mrea_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mrea::Application.config.session_store :active_record_store
